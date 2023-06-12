ce89ecc4fb01bd155302ead4d129d86039437d06
dilan besong <besongdilan@gmail.com>
2023-06-12 14:34:29 +0100

    added dialogue box

--- auth.js
+++ auth.js
@@ -3,9 +3,20 @@ const navBar = document.querySelector('.auth_nav')
 const form = document.querySelector(".form");
 const navButtonContainer = document.querySelector('ul')
 const navToggleButton = document.querySelector('i') 
+const dialogueBox = document.querySelector(".auth_dialog_Box");
+const closeDialogButton = dialogueBox.querySelector(".close_btn");
 
 // fa fa-times or fa fa-bars
 console.log(navToggleButton.classList.contains("fa-bars"));
+
+closeDialogButton.addEventListener('click', () => {
+dialogueBox.classList.remove("open-popup");
+})
+form.addEventListener('submit', e => {
+   e.preventDefault()
+   dialogueBox.classList.add("open-popup");
+})
+
 navToggleButton.addEventListener('click', (e) => {
   navBar.classList.toggle("inc_nav_height");
      if(e.target.classList.contains('fa-bars')){
